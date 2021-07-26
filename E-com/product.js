f9cbb291434f9122254e5e93b3a9c743f7444875
ayman bin Mannan <83162388+ayman2943@users.noreply.github.com>
2021-07-26 19:18:09 +0600

    nothing

---
+++ E-com/product.js
@@ -0,0 +1,409 @@
+
+
+var menuBtn = document.getElementById("menu") 
+var leftMenu = document.querySelector('#side-bar-left')
+
+
+menuBtn.addEventListener("click", function(){
+    leftMenu.style.transition = "transform .5s ease-in-out"
+      leftMenu.classList.toggle('active')
+      if(leftMenu.classList.contains('active')){
+          menuBtn.style.color ='black'
+      }else{
+        menuBtn.style.color ='white'
+      }
+  
+})
+
+var homeBtn = document.querySelectorAll(".home-btn")
+var categoryBtn = document.querySelectorAll(".category-btn")
+homeBtn.forEach(function(btn){
+  btn.addEventListener('click',function(){
+  window.location.href=`index.html#home`
+})})
+categoryBtn.forEach(function(btn){
+  btn.addEventListener('click',function(){
+  window.location.href=`index.html#category`
+})
+})
+
+ 
+    
+
+ 
+
+
+
+
+//             account
+
+    var accountBtn = document.querySelector('#account')
+    var accountContainer = document.querySelector('#account-container')
+    
+accountBtn.addEventListener('click',function(){
+      accountContainer.classList.toggle('active')
+    })
+
+document.addEventListener('click',function(e){
+  var x= e.target
+  if (!accountContainer.contains(x) && !accountBtn.contains(x)){
+  accountContainer.classList.remove('active')
+
+}
+  
+})
+
+
+     var login = document.querySelector('#login')
+     var register = document.querySelector('#register')
+     var loginContainer = document.querySelector('#login-container')
+     var registerContainer = document.querySelector('#register-container') 
+
+     login.addEventListener('click',function(){
+      registerContainer.classList.remove('active')
+      loginContainer.classList.add('active')
+      login.classList.add('active')
+      register.classList.remove('active')
+     })
+     register.addEventListener('click',function(){
+      loginContainer.classList.remove('active')
+      registerContainer.classList.add('active')
+      register.classList.add('active')
+      login.classList.remove('active')
+     })
+
+
+
+    //  cart
+
+    var cartBtn = document.querySelector('#cart') 
+    var cartsContainer = document.querySelector('#carts-container')
+    var cartImage = document.querySelector('.cartImage')
+    var cartName = document.querySelector('.cartName')
+    var cartPrice = document.querySelector('.cartPrice')
+    var carts = document.querySelector('#carts')
+    var cartDatas =JSON.parse(localStorage.getItem('cart'))
+    
+    update()
+    
+    function update(){
+     
+    if (cartDatas){
+      cartDatas.forEach(function cartDataName(cartData){
+        var cart = document.createElement('div')
+        cart.classList.add('cart')
+        cart.innerHTML =`
+        <div class="cart-container">
+         <div class="cart-image" style="background-image:url('${cartData.image}')"></div>
+         <div class="cart-info"> 
+         <div class="cart-name">${cartData.name}</div>
+         <div class="cart-price">${cartData.price}</>$</div>
+         <div class="cart-remove">Remove</div>
+      </div></div>
+      <div class="cart-product-input">
+      <button class="up-input"><i class="fas fa-chevron-up"></i></button>
+      <input type="number"  value="1" min="1">
+          <button class="down-input"><i class="fas fa-chevron-down"></i></button>
+          </div>`
+         carts.appendChild(cart)
+
+         removeLS()
+         console.log(cartDatas)
+          
+      })
+    }else if(cartDatas=""){
+      localStorage.removeItem('cart')
+    }
+     
+  }
+
+    function createCart(){
+      var cart = document.createElement('div')
+      cart.classList.add('cart')
+      cart.innerHTML =`
+      <div class="cart-container">
+       <div class="cart-image" style="background-image:url('${product.image}')"></div>
+       <div class="cart-info"> 
+       <div class="cart-name">${product.name}</div>
+       <div class="cart-price">${product.price}</>$</div>
+       <div class="cart-remove">Remove</div>
+    </div></div>
+    <div class="cart-product-input">
+      <button class="up-input"><i class="fas fa-chevron-up"></i></button>
+      <input type="number"  value="1" min="1">
+          <button class="down-input"><i class="fas fa-chevron-down"></i></button>
+          </div>`
+       carts.appendChild(cart)
+            addRemove()
+            removeLS()
+     
+    }
+
+
+    cartBtn.addEventListener('click',viewCart)
+
+    function viewCart(){
+      cartsContainer.classList.toggle('active')
+    }
+
+  document.addEventListener('click',function(e){
+  var x= e.target
+  if (!cartsContainer.contains(x) && !cartBtn.contains(x) && spCart != x) {
+  cartsContainer.classList.remove('active')
+
+}
+  
+})
+function increaseNumber(){
+  var count =1;
+  count =  this.parentElement.querySelector("input").value 
+ count++;
+ this.parentElement.querySelector("input").value = count;
+}
+
+function updateInput(){
+
+var cart_upBtn = document.querySelectorAll(".cart .up-input")
+var cart_downBtn = document.querySelectorAll(".cart .down-input")
+//  var count = 1;
+ cart_upBtn.forEach(function(btn){
+ 
+    btn.addEventListener('click',function(){
+      var count = this.parentElement.querySelector("input").value;
+      
+     count++;
+     this.parentElement.querySelector("input").value = count;
+     
+
+      countTotal();
+    }) })
+    
+ cart_downBtn.forEach(function(btn){
+ 
+  btn.addEventListener('click',function(){
+    var count = this.parentElement.querySelector("input").value 
+    console.log(count)
+    count--;
+    if(count === 0){
+      count =1;
+    }
+    this.parentElement.querySelector("input").value = count;
+    console.log(count)
+    countTotal();
+    
+  })
+})
+ 
+
+}
+// updateInput();
+// ----------------------single-product----
+
+
+
+var spImage = document.querySelector(".single-product-image")
+var spSize = document.querySelector(".single-product-size")
+var spPrice = document.querySelector(".single-product-price")
+var spCart = document.querySelector(".add-cart")
+var spName = document.querySelector(".single-product-info h1")
+var upBtn = document.querySelector(".single-product-input .up-input")
+var downBtn = document.querySelector(".single-product-input .down-input")
+var input = document.querySelector(".single-product-number")
+var clearCart = document.querySelector("#cart-clear")
+var numberOfProduct =1;
+
+
+function updateNumber(){
+  input.value = numberOfProduct
+}
+upBtn.addEventListener('click', function (){
+  numberOfProduct++;
+  updateNumber()
+  
+})
+downBtn.addEventListener('click',function(){
+  numberOfProduct--;
+  if (numberOfProduct<=0){
+    numberOfProduct =1;
+  }
+  updateNumber()
+})
+const product = JSON.parse(localStorage.getItem('product'))
+
+
+spImage.style.backgroundImage = `url("${product.image}")`
+spPrice.innerHTML = `${product.price}$`
+spName.innerHTML = `${product.name}`
+
+
+
+
+spCart.addEventListener('click',function(){
+   
+  if (localStorage.getItem("cart") === null){
+    updateLS();
+    createCart();   
+    updateInput();
+    cartsContainer.classList.add('active');
+   
+  }else{
+    var cartDatas =JSON.parse(localStorage.getItem('cart'))
+    
+
+  var lessThan = cartDatas.some(function(cardata){
+     return product.id === cardata.id
+  })
+
+  if(lessThan === true){
+    var cart_input = document.querySelectorAll(".cart input")
+      cart_input.forEach(function(btn){
+         var t= btn.parentElement.parentElement
+        if(t.innerHTML.indexOf(product.name)!= -1){
+          var counter = btn.value
+          counter ++;
+          btn.value = counter;
+          // numberOfCart();
+        }
+      
+      })
+   
+ 
+  }
+  else{
+    pushLS(cartDatas)
+    inLS(cartDatas)
+    // updateLS()
+    createCart();
+    updateInput();
+  
+    cartsContainer.classList.add('active');
+  }
+   }
+   countTotal()
+    
+ 
+})
+
+
+
+
+function pushLS(cartProducts){
+  cartProducts.push(product)
+}
+function inLS(cartProducts){
+  localStorage.setItem('cart',JSON.stringify(cartProducts))
+}
+
+function updateLS(){
+      var cartProducts = [];
+       pushLS(cartProducts)
+       inLS(cartProducts)
+          
+       
+
+}
+
+
+
+
+
+
+
+clearCart.addEventListener('click',function(){
+     localStorage.setItem('cart',[])
+     localStorage.removeItem('cart')
+  var cart = document.querySelectorAll(".cart")
+  if (cart){
+    cart.forEach(function(c){
+       c.remove();
+    })
+  }
+  // update()
+  countTotal()
+})
+
+
+
+
+ countTotal()
+function countTotal(){
+  var cart_input = document.querySelectorAll(".cart input") 
+  var cart_total =document.getElementById("cart-total")
+   var y = []
+    cart_input.forEach(function(as){ 
+    var x = as.parentElement.parentElement.querySelector(".cart-price").innerText;
+   
+    y.push(parseInt(x) * parseInt(as.value))
+    var total = y.reduce((a,b)=>a+b, 0)
+    
+     cart_total.innerHTML= `Your total :${total}$`
+  }) 
+  if(cart_input.length == 0){
+    cart_total.innerHTML= `Your total : 00$`
+  }
+ 
+ 
+  
+    
+    
+}
+
+
+
+
+
+
+function addRemove(){
+  var removeBtn = document.querySelectorAll(".cart-remove")
+  removeBtn.forEach(function(btn){
+    btn.addEventListener("click", function(){
+      this.parentElement.parentElement.parentElement.remove();
+      setTimeout(() => {
+        cartsContainer.classList.add('active');
+      }, 1);
+      countTotal();
+      // update()
+    })
+  })
+  
+  } 
+
+
+  
+
+
+  function removeLS(){
+    var removeBtn = document.querySelectorAll(".cart-remove")
+   
+    removeBtn.forEach(function(btn){
+
+  btn.addEventListener("click", function(){
+    var cartDatas =JSON.parse(localStorage.getItem('cart'))
+    
+
+    var lessThan = cartDatas.some(function(cardata){
+       return product.id === cardata.id
+    })
+    cartDatas.forEach(function(carData){
+      if(lessThan = true){
+        const index = cartDatas.indexOf(carData);
+              if (index > -1) {
+                cartDatas.splice(index, 1);
+                console.log(cartDatas)
+                setTimeout(() => {
+                  localStorage.setItem('cart',JSON.stringify(cartDatas))
+                }, 1);
+            }
+      }
+    })
+ 
+  })
+    })}
+    
+var crossBtn = document.querySelector("#cross")
+
+
+crossBtn.addEventListener("click", function(){
+  cartsContainer.classList.remove('active')
+})
+
---
+++ homepage/script.js
@@ -0,0 +1,225 @@
+var menuBtn = document.querySelector("#menu-btn")
+var firstBtn = document.querySelector("#first-btn")
+var secondBtn = document.querySelector("#second-btn")
+var menu = document.querySelector('#navbar ul')
+
+menuBtn.addEventListener("click", function(){
+   firstBtn.classList.toggle("hidden")
+   secondBtn.classList.toggle("hidden")
+   menu.classList.toggle("collapsed")
+})
+
+
+
+
+
+
+ var home = document.querySelector(".home")
+var works = document.querySelector('.works')
+var about = document.querySelector('.about')
+var skills = document.querySelector('.skills')
+var leftHome = document.querySelector('#home-left')
+var rightHome = document.querySelector('#home-right')
+var rightHomeBig = document.querySelector('#home-right #right-cross')
+var rightHomeText = document.querySelector('#home-right p')
+var homePage = document.querySelector("#homepage")
+
+works.addEventListener('click',function(){
+           removeHome()
+           removeSkill()
+          
+         setTimeout(() => {
+            bringWork()
+         }, 250);   
+         workBox.addEventListener('click',function(){
+            window.location.href = "../E-com/index.html"
+         }) 
+         
+})
+home.addEventListener('click',function(){
+   
+   // homePage.style.display=``
+   removeWork()
+   removeSkill()
+   
+   setTimeout(() => {
+      bringHome()
+   }, 250); 
+
+})
+skills.addEventListener("click",function(){
+   removeWork()
+   removeHome()
+   setTimeout(() => {
+      bringSkill();
+   }, 250); 
+  
+})
+
+
+
+
+
+
+//  functions
+
+function removeHome(){
+   leftHome.style.transform =` translateX(-100%)`
+   leftHome.style.transition = ` all .5s ease-in-out`
+   rightHomeBig.style.transform = `translateX(100%)`
+   rightHomeBig.style.transition = ` all .5s ease-in-out`
+   setTimeout(() => {
+      rightHomeText.style.display = `none`
+   }, 250);
+   setTimeout(() => {
+      rightHome.style.transform = `translateX(100%)`
+   rightHome.style.transition = ` all .5s ease-in-out`
+      
+   },500);
+   setTimeout(() => {
+      homePage.style.display=`none`
+   }, 450);
+}
+
+function bringHome(){
+   homePage.style.display = ``
+   setTimeout(() => {
+      leftHome.style.transform =` translateX(0)`
+      leftHome.style.transition = ` all 1s ease-in-out`
+   }, 100);
+  
+   rightHome.style.transform = ``
+   rightHome.style.transition = `all .5s ease-in-out`
+   setTimeout(() => {
+      rightHomeText.style.display = ``
+   }, 500);
+   setTimeout(() => { 
+      mediaOfHome()
+      window.addEventListener("resize",mediaOfHome )
+   },100);
+}
+
+
+// media query
+function mediaOfHome(){
+   if(window.innerWidth>991){
+      rightHomeBig.style.transform = `translateX(0%) skewX(50deg)`
+      rightHomeBig.style.transition = ` transform 1s ease-in-out`
+   } else if(window.innerWidth>767&& window.innerWidth<=991){
+      rightHomeBig.style.transform = `translateX(0%) skewX(40deg)`
+      rightHomeBig.style.transition = ` transform 1s ease-in-out`
+   }
+   else if(window.innerWidth>575 && window.innerWidth<=767){
+      rightHomeBig.style.transform = `translateX(0%) skewX(30deg)`
+      rightHomeBig.style.transition = ` transform 1s ease-in-out`
+   }
+    else if(window.innerWidth>299 && window.innerWidth<= 575){
+      rightHomeBig.style.transform = `translateX(0%) skewX(20deg)`
+      rightHomeBig.style.transition = ` transform 1s ease-in-out`
+   }else{
+      rightHomeBig.style.transform = `translateX(0%) skewX(15deg)`
+      rightHomeBig.style.transition = ` transform 1s ease-in-out`
+   }
+}
+function mediaOfWork(){
+   if(window.innerWidth>991){
+      workCross.style.transform = ` skewX(-15deg)`
+      workCross.style.transition = ` transform .5s ease-in-out`
+   } else if(window.innerWidth>767&& window.innerWidth<=991){
+      
+
+      workCross.style.transform = ` skewX(-10deg)`
+      workCross.style.transition = ` transform .5s ease-in-out`
+   }
+   else if(window.innerWidth>575 && window.innerWidth<=767){
+      workCross.style.transform = ` skewX(-5deg)`
+      workCross.style.transition = ` transform .5s ease-in-out`
+   }
+    else if(window.innerWidth>299 && window.innerWidth<= 575){
+      workCross.style.transform = ` skewX(-4deg)`
+      workCross.style.transition = ` transform .5s ease-in-out`
+   }
+   else if(window.innerWidth<=299){
+     
+      workCross.style.transform = ` skewX(-3deg)`
+      workCross.style.transition = ` transform .5s ease-in-out`
+   }
+}
+function mediaOfSkill(){
+   if(window.innerWidth>991){
+      skillCross.style.transform = ` skewX(15deg)`
+      skillCross.style.transition = ` transform .5s ease-in-out`
+   } else if(window.innerWidth>767&& window.innerWidth<=991){
+      
+
+      skillCross.style.transform = ` skewX(10deg)`
+      skillCross.style.transition = ` transform .5s ease-in-out`
+   }
+   else if(window.innerWidth>575 && window.innerWidth<=767){
+      skillCross.style.transform = ` skewX(5deg)`
+      skillCross.style.transition = ` transform .5s ease-in-out`
+   }
+    else if(window.innerWidth>299 && window.innerWidth<= 575){
+      skillCross.style.transform = ` skewX(5deg)`
+      skillCross.style.transition = ` transform .5s ease-in-out`
+   }
+   else if(window.innerWidth<=299){
+     
+      skillCross.style.transform = ` skewX(4deg)`
+      skillCross.style.transition = ` transform .5s ease-in-out`
+   }
+}
+
+
+
+// work
+
+
+var workPage =document.querySelector('#workpage')
+var workCross = document.querySelector("#work-cross")
+var workBox = document.querySelector(".workBox")
+function bringWork(){
+   workPage.style.display=``
+   setTimeout(function(){
+      workPage.classList.add("active")
+   menuBtn.style.color = `black`
+   },100)
+   
+   mediaOfWork()
+   window.addEventListener("resize",mediaOfWork)
+
+}
+function removeWork(){
+   workPage.classList.remove("active")
+   menuBtn.style.color = ``
+   setTimeout(() => {
+      workPage.style.display=`none`
+   }, 450);
+}
+
+// skills
+
+var skillPage =document.querySelector('#skillPage')
+var skillCross = document.querySelector("#skillCross")
+
+function bringSkill(){
+   skillPage.style.display=``
+   setTimeout(function(){
+      skillPage.classList.add("active")
+  
+   },100)
+   
+   mediaOfSkill()
+   window.addEventListener("resize",mediaOfSkill)
+
+}
+function removeSkill(){
+   skillPage.classList.remove("active")
+ 
+   setTimeout(() => {
+      skillPage.style.display=`none`
+   }, 450);
+}
+
+
+
